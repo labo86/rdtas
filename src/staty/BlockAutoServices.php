0970e575b62acaa0966224de846da9479780b2fa
Edwin <edwrodrig@gmail.com>
2020-09-11 08:52:11 -0300

    Adding new things!

--- src/staty/BlockAutoServices.php
+++ src/staty/BlockAutoServices.php
@@ -23,6 +23,8 @@ class BlockAutoServices extends Block
 {
     protected string $services;
 
+    protected array $custom_method_form_list = [];
+
     public function setService(string $service) {
         $this->service = $service;
     }
@@ -31,8 +33,44 @@ class BlockAutoServices extends Block
         return $this->service;
     }
 
+    public function sectionBeginForm($method) {
+        $form_data = [
+                'method' => $method,
+                'id' => $method . '_custom'
+        ];
+
+        $this->custom_method_form_list[] = $form_data;
+        $this->sectionBegin($form_data['id']);
+    }
+
     public function html() {?>
 <div id="main-container" class="section-container" style="text-align:center">
+        <div class="container-padding" data-page-name="index_page">
+            <h2>Servicios personalizados</h2>
+            <?php foreach ( $this->custom_method_form_list as $form_data ) :
+                $method = $form_data['method'];
+                $id = $form_data['id'];
+                ?>
+                <button onclick="changePage('<?=$id?>_page')"><?=$method?></button><br/>
+            <?php endforeach; ?>
+            <h2>Servicios automaticos disponibles</h2>
+            <div id="automatic_buttons"></div>
+        </div>
+    <?php foreach ( $this->custom_method_form_list as $form_data ) :
+        $method = $form_data['method'];
+        $id = $form_data['id'];
+        ?>
+        <div class="container-padding" data-page-name="<?=$id?>_page" style="display:none">
+            <h2><?=$method?></h2>
+            <form id="<?=$id?>_form">
+            <?=$this->getSectionContent($id)?>
+                <br/>
+                <input type="hidden" name="method" value="<?=$method?>">
+                <button onclick="submitRequest('<?=$id?>_form')">Enviar</button>
+            </form>
+            <button onclick="changePage('index_page')">Back</button>
+        </div>
+    <?php endforeach; ?>
 </div>
 <script>
 const endpoint = '<?=$this->getService()?>';
@@ -40,47 +78,46 @@ const endpoint = '<?=$this->getService()?>';
 fetch(endpoint + "?method=get_automatic_method_list")
 .then(response  => response.json())
 .then(function(myJson) {
-            let html = "    <div class=\"container-padding\" data-page-name=\"index_page\">\n" +
-                "        <h2>Servicios disponibles</h2>\n";
 
-    for ( let automatic_method of myJson )
-        if ( automatic_method.parameter_list.length > 0 ) {
-            html += "        <button onclick=\"changePage('" + automatic_method.method + "_page')\">" + automatic_method.method + "</button><br/>";
-        } else {
-            html += "        <button onclick=\"submitRequestGet('method=" + automatic_method.method + "')\">" + automatic_method.method + "</button><br/>";
-        }
-
-    html += "    </div>";
-
+    let html = "";
     for ( let automatic_method of myJson ) {
-                if ( automatic_method.parameter_list.length === 0 ) continue;
-                html += "    <div class=\"container-padding\" data-page-name=\"" + automatic_method.method + "_page\" style=\"display:none\">\n" +
-                    "        <h2>" + automatic_method.method +"</h2>\n" +
-                    "        <form id=\"" + automatic_method.method +"_form\">\n";
+            if ( automatic_method.parameter_list.length === 0 ) continue;
+            html += "    <div class=\"container-padding\" data-page-name=\"" + automatic_method.method + "_page\" style=\"display:none\">\n" +
+                "        <h2>" + automatic_method.method +"</h2>\n" +
+                "        <form id=\"" + automatic_method.method +"_form\">\n";
 
-                for ( let parameter of automatic_method.parameter_list) {
-                    html += "<label>" + parameter.name + "</label>";
-                    if ( parameter.type === 'labo86\\hapi\\InputFile' ) {
-                        html += "<input type=\"file\" name=\"" + parameter.name + "\">";
-                    } else if ( parameter.type === 'labo86\\hapi\\InputFileList') {
-                        html += "<input type=\"file\" name=\"" + parameter.name + "\" multiple>";
-                    } else if ( parameter.type === 'string') {
-                        html += "<input type=\"text\" name=\"" + parameter.name + "\">";
-                    } else if ( parameter.type === 'int') {
-                        html += "<input type=\"text\" name=\"" + parameter.name + "\">";
-                    }
-                    html += "<br/>";
+            for ( let parameter of automatic_method.parameter_list) {
+                html += "<label>" + parameter.name + "</label>";
+                if ( parameter.type === 'labo86\\hapi\\InputFile' ) {
+                    html += "<input type=\"file\" name=\"" + parameter.name + "\">";
+                } else if ( parameter.type === 'labo86\\hapi\\InputFileList') {
+                    html += "<input type=\"file\" name=\"" + parameter.name + "\" multiple>";
+                } else if ( parameter.type === 'string') {
+                    html += "<input type=\"text\" name=\"" + parameter.name + "\">";
+                } else if ( parameter.type === 'int') {
+                    html += "<input type=\"text\" name=\"" + parameter.name + "\">";
                 }
+                html += "<br/>";
+            }
 
         html +=
-            "            <input type=\"hidden\" name=\"method\" value=\""+ automatic_method.method + "\">\n" +
-            "            <button onclick=\"submitRequest('"+ automatic_method.method + "_form')\">Enviar</button>\n" +
-            "        </form>\n" +
-            "        <button onclick=\"changePage('index_page')\">Back</button>\n" +
-            "    </div>";
+        "            <input type=\"hidden\" name=\"method\" value=\""+ automatic_method.method + "\">\n" +
+        "            <button onclick=\"submitRequest('"+ automatic_method.method + "_form')\">Enviar</button>\n" +
+        "        </form>\n" +
+        "        <button onclick=\"changePage('index_page')\">Back</button>\n" +
+        "    </div>";
     }
 
-    document.getElementById('main-container').innerHTML = html;
+    let html_automatic_buttons = "";
+    for ( let automatic_method of myJson )
+        if ( automatic_method.parameter_list.length > 0 ) {
+            html_automatic_buttons += "        <button onclick=\"changePage('" + automatic_method.method + "_page')\">" + automatic_method.method + "</button><br/>";
+        } else {
+            html_automatic_buttons += "        <button onclick=\"submitRequestGet('method=" + automatic_method.method + "')\">" + automatic_method.method + "</button><br/>";
+    }
+    document.getElementById('automatic_buttons').innerHTML = html_automatic_buttons;
+
+    appendToInnerHtml(document.getElementById('main-container'), html);
 
     let url = new URL(window.location);
     let params = new URLSearchParams(url.search);
@@ -91,6 +128,15 @@ fetch(endpoint + "?method=get_automatic_method_list")
 
 
 });
+
+function appendToInnerHtml(element, content) {
+    let new_content = document.createElement('div');
+    new_content.innerHTML = content;
+
+    while (new_content.firstChild) {
+        element.appendChild(new_content.firstChild);
+    }
+}
 
 function switchVisibility(element, name) {
     for ( let child of element.children ) {
