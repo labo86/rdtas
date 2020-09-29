e5d26cc245c81b65a927f1a9efc5bf2374f02ccc
Edwin <edwrodrig@gmail.com>
2020-09-29 09:53:33 -0300

    User data

---
+++ src/app/User.php
@@ -0,0 +1,222 @@
+<?php
+declare(strict_types=1);
+
+
+namespace labo86\rdtas\app;
+
+
+use DateInterval;
+use DateTime;
+use labo86\exception_with_data\ExceptionWithData;
+use labo86\exception_with_data\Util;
+use labo86\rdtas\pdo\Util as UtilPDO;
+use PDO;
+use Throwable;
+
+class User
+{
+
+    const SESSION_TABLE_DDL = <<<EOF
+create table sessions
+(
+	session_id varchar(36) not null
+		primary key,
+	user_id varchar(36) null,
+	creation_date datetime null,
+	expiration_date datetime null,
+	state varchar(36) null
+);
+EOF;
+
+    const USER_TABLE_DDL = <<<EOF
+create table users
+(
+	user_id varchar(36) not null
+		primary key,
+	name varchar(36) null,
+	password_hash varchar(255) null,
+	email varchar(100) null,
+	type varchar(10) null
+);
+EOF;
+
+
+    const DATE_FORMAT = "Y-m-d H:i:s";
+
+    public static function getSession(PDO $pdo, string $session_id) : array {
+
+        try {
+            $row = UtilPDO::selectRow($pdo, 'SELECT user_id, creation_date, expiration_date, state FROM sessions WHERE session_id = :session_id', [
+                'session_id' => $session_id
+            ]);
+
+            return $row;
+        } catch ( Throwable $exception ) {
+            throw Util::rethrow('session does not exists', [
+                'session_id' => $session_id
+            ], $exception);
+
+        }
+    }
+
+    public static function getUser(PDO $pdo, string $user_id) : array {
+        try {
+            $row = UtilPDO::selectRow($pdo, 'SELECT user_id, name, type FROM users WHERE user_id = :user_id', [
+                'user_id' => $user_id
+            ]);
+
+            return $row;
+        } catch ( Throwable $exception ) {
+            throw Util::rethrow('user does not exists', [
+                'user_id' => $user_id
+            ], $exception);
+        }
+
+    }
+
+    public static function createUser(PDO $pdo, string $user_id, string $username, string $password_hash) : array {
+        UtilPDO::updateOne($pdo,"INSERT INTO users (user_id, name, password_hash, type) VALUES (:user_id, :name, :password_hash, :type)", [
+            'user_id' => $user_id,
+            'name' => $username,
+            'password_hash' => $password_hash,
+            'type' => 'REGISTERED'
+        ]);
+        return [];
+    }
+
+    public static function validateUserType(PDO $pdo, string $user_id, string $required_type) : array {
+        $user = self::getUser($pdo, $user_id);
+        $type = $user['type'];
+        if ( $user['type'] !== $required_type ) {
+            throw new ExceptionWithData('user does not have permissions', [
+                'user_id' =>  $user_id,
+                'required_type' => $required_type,
+                'user_type' => $type
+            ]);
+        }
+        return $user;
+    }
+
+    public static function createSession(PDO $pdo, string $username, string $password) : array {
+        try {
+            $row = UtilPDO::selectRow($pdo, "SELECT user_id, name, password_hash FROM users WHERE name = :name", [
+                'name' => $username
+            ]);
+        } catch ( Throwable $exception ) {
+            throw Util::rethrow('user does not exists', [
+                'username' => $username
+            ], $exception);
+        }
+
+        $session_id = md5(microtime());
+        $password_hash = $row['password_hash'];
+        if ( !password_verify($password, $password_hash) ) {
+            throw new ExceptionWithData('wrong password', [
+                'username' => $username,
+                'password' => $password
+            ]);
+        }
+
+        $user_id = $row['user_id'];
+        $date = new DateTime();
+        $creation_date = $date->format(self::DATE_FORMAT);
+        $date->add(new DateInterval('P1D'));
+        $expiration_date =  $date->format(self::DATE_FORMAT);
+        UtilPDO::updateOne($pdo, 'INSERT INTO sessions (session_id, user_id, creation_date, expiration_date, state) VALUES (:session_id, :user_id, :creation_date, :expiration_date, :state)',
+            [
+                'session_id' => $session_id,
+                'user_id' => $user_id,
+                'creation_date' => $creation_date,
+                'expiration_date' => $expiration_date,
+                'state' => 'ACTIVE'
+            ]);
+
+        return [
+            'session_id' => $session_id,
+            'user_id' => $user_id,
+            'creation_date' => $creation_date,
+            'expiration_date' => $expiration_date,
+            'state' => 'ACTIVE'
+        ];
+    }
+
+    public static function validateSession(PDO $pdo, string $session_id) : array {
+        $session = self::getSession($pdo, $session_id);
+        $user_id = $session['user_id'];
+        $state = $session['state'];
+        if ( $state !== 'ACTIVE' )
+            throw new ExceptionWithData('session is not active', [
+                'session_id' => $session_id,
+                'user_id' => $user_id,
+                'state' => $state
+            ]);
+
+
+        $expiration_date = DateTime::createFromFormat(self::DATE_FORMAT, $session['expiration_date']);
+        $current_date = new DateTime();
+        if ( $expiration_date < $current_date ) {
+            throw new ExceptionWithData('session is expired', [
+                'session_id' => $session_id,
+                'user_id' => $user_id,
+                'state' => $state,
+                'current_date' => $current_date->format(self::DATE_FORMAT),
+                'expiration_date' => $expiration_date->format(self::DATE_FORMAT)
+            ]);
+        }
+
+        return $session;
+    }
+
+    /**
+     * Esta funcion {@see validateSession() valida} y obtiene el user id
+     * @param PDO $pdo
+     * @param string $session_id
+     * @return string
+     * @throws ExceptionWithData
+     */
+    public static function getUserIdFromSessionId(PDO $pdo, string $session_id) : string {
+        $session = self::validateSession($pdo, $session_id);
+        return $session['user_id'];
+    }
+
+    /**
+     * @param PDO $pdo
+     * @param string $session_id
+     * @return array la informacion del usuario
+     * @throws ExceptionWithData
+     */
+    public static function validateAdminFromSessionId(PDO $pdo, string $session_id) : array {
+        $user_id = self::getUserIdFromSessionId($pdo, $session_id);
+        return self::validateUserType($pdo, $user_id, 'ADMIN');
+    }
+
+    public static function closeSession(PDO $pdo, string $session_id) {
+        UtilPDO::updateOne($pdo, 'UPDATE sessions SET state = :state WHERE session_id = :session_id',
+            [
+                'state' => 'CLOSED',
+                'session_id' => $session_id
+        ]);
+    }
+
+    public static function setUserType(PDO $pdo, string $user_id, string $type) {
+        try {
+            UtilPDO::updateOne($pdo, 'UPDATE users SET type = :type WHERE user_id = :user_id',
+                [
+                    'type' => $type,
+                    'user_id' => $user_id
+                ]);
+        } catch (ExceptionWithData $exception ) {
+            var_dump($exception->getData());
+        }
+    }
+
+
+    public static function setUserPassword(PDO $pdo, string $user_id, string $password_hash) {
+        UtilPDO::updateOne($pdo, 'UPDATE users SET password_hash = :password_hash WHERE user_id = :user_id',
+            [
+                'password_hash' => $password_hash,
+                'user_id' => $user_id
+            ]);
+    }
+
+}
---
+++ test/app/UserTest.php
@@ -0,0 +1,209 @@
+<?php
+declare(strict_types=1);
+
+namespace test\labo86\rdtas\app;
+
+use labo86\exception_with_data\ExceptionWithData;
+use labo86\rdtas\app\User;
+use labo86\rdtas\pdo\Util;
+use PDO;
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+
+
+    public function getPDO() : PDO {
+        $pdo = new PDO(Util::sqliteDns(":memory:"));
+        Util::update($pdo, User::USER_TABLE_DDL);
+        Util::update($pdo,  User::SESSION_TABLE_DDL);
+        $user_id = 'test';
+        $username = 'test';
+        $password_hash = password_hash('pass',  PASSWORD_DEFAULT);
+        User::createUser($pdo, $user_id, $username, $password_hash);
+        return $pdo;
+    }
+
+    public function testGetSession()
+    {
+        $pdo = $this->getPDO();
+        $session = User::createSession($pdo,'test', 'pass');
+        $session_again = User::getSession($pdo, $session['session_id']);
+        unset($session['session_id']);
+        $this->assertEquals($session, $session_again);
+    }
+
+    public function testGetSessionUnexistent()
+    {
+        $this->expectExceptionMessage('session does not exist');
+        try {
+            $pdo = $this->getPDO();
+            User::getSession($pdo,'test');
+
+            $this->fail('This should fail');
+        } catch ( ExceptionWithData $exception ) {
+            $data = $exception->getData();
+            $this->assertEquals('test', $data['session_id']);
+            throw $exception;
+        }
+    }
+
+    public function testValidateSessionClosed()
+    {
+        $this->expectExceptionMessage('session is not active');
+        try {
+            $pdo = $this->getPDO();
+            $session = User::createSession($pdo,'test', 'pass');
+            User::closeSession($pdo, $session['session_id']);
+
+            User::validateSession($pdo, $session['session_id']);
+
+            $this->fail('This should fail');
+        } catch ( ExceptionWithData $exception ) {
+            $data = $exception->getData();
+            $this->assertArrayHasKey('session_id', $data);
+            unset($data['session_id']);
+            $this->assertEquals([
+                'user_id' => 'test',
+                'state' => 'CLOSED'
+            ], $data);
+            throw $exception;
+        }
+    }
+
+    public function testValidateSession()
+    {
+        $pdo = $this->getPDO();
+        $session = User::createSession($pdo,'test', 'pass');
+        $result = User::validateSession($pdo, $session['session_id']);
+        unset($session['session_id']);
+        $this->assertEquals($session, $result);
+    }
+
+    public function testGetUserIdFromSessionId()
+    {
+        $pdo = $this->getPDO();
+        $session = User::createSession($pdo,'test', 'pass');
+        $user_id = User::getUserIdFromSessionId($pdo, $session['session_id']);
+        $this->assertEquals('test', $user_id);
+    }
+
+    public function testValidateUserType()
+    {
+        $pdo = $this->getPDO();
+
+        $user = User::validateUserType($pdo, 'test', 'REGISTERED');
+        $this->assertEquals([
+            'user_id' => 'test',
+            'name' => 'test',
+            'type' => 'REGISTERED'
+        ], $user);
+    }
+
+    public function testValidateUserTypeInvalid() {
+        $this->expectExceptionMessage('user does not have permissions');
+        try {
+            $pdo = $this->getPDO();
+            User::validateUserType($pdo, 'test', 'ADMIN');
+
+            $this->fail('This should fail');
+        } catch ( ExceptionWithData $exception ) {
+            $data = $exception->getData();
+            $this->assertEquals([
+                'user_id' => 'test',
+                'user_type' => 'REGISTERED',
+                'required_type' => 'ADMIN'
+            ], $data);
+            throw $exception;
+        }
+
+    }
+
+    public function testCreateSession()
+    {
+        $pdo = $this->getPDO();
+        $session = User::createSession($pdo,'test', 'pass');
+        $this->assertEquals('test', $session['user_id']);
+        $user = User::getUser($pdo, $session['user_id']);
+        $this->assertEquals('test', $user['user_id']);
+        $this->assertEquals('test', $user['name']);
+    }
+
+    public function testCreateSessionWrongPassword()
+    {
+        $this->expectExceptionMessage('wrong password');
+        try {
+            $pdo = $this->getPDO();
+            User::createSession($pdo,'test', 'wrong');
+            $this->fail('This should fail');
+        } catch ( ExceptionWithData $exception ) {
+            $data = $exception->getData();
+            $this->assertEquals([
+                'username' => 'test',
+                'password' => 'wrong'
+            ], $data);
+            throw $exception;
+        }
+    }
+
+    public function testValidateAdminFromSessionId()
+    {
+        $pdo = $this->getPDO();
+        User::setUserType($pdo, 'test', 'ADMIN');
+
+        $session = User::createSession($pdo, 'test', 'pass');
+        $user = User::validateAdminFromSessionId($pdo, $session['session_id']);
+        $this->assertEquals([
+            'user_id' => 'test',
+            'name' => 'test',
+            'type' => 'ADMIN'
+        ], $user);
+    }
+
+    public function testValidateAdminFromSessionIdNotValid()
+    {
+        $this->expectExceptionMessage('user does not have permissions');
+        try {
+            $pdo = $this->getPDO();
+
+            $session = User::createSession($pdo, 'test', 'pass');
+            User::validateAdminFromSessionId($pdo, $session['session_id']);
+
+            $this->fail('This should fail');
+        } catch ( ExceptionWithData $exception ) {
+            $data = $exception->getData();
+            $this->assertEquals([
+                'required_type'=> 'ADMIN',
+                'user_type' => 'REGISTERED',
+                'user_id' => 'test'
+            ], $data);
+            throw $exception;
+        }
+
+    }
+
+    public function testCreateUser()
+    {
+        $pdo = $this->getPDO();
+        $user_id = 'a';
+        $username = 'test2';
+        $password_hash = password_hash('pass',  PASSWORD_DEFAULT);
+
+        User::createUser($pdo, $user_id, $username, $password_hash);
+
+        $user = User::getUser($pdo, $user_id);
+        $this->assertEquals($user['name'], $username);
+    }
+
+    public function testUserPassword() {
+        $pdo = $this->getPDO();
+        $session = User::createSession($pdo, 'test', 'pass');
+        $password_hash = password_hash('pass2',  PASSWORD_DEFAULT);
+        User::setUserPassword($pdo, 'test', $password_hash);
+
+        $session2 = User::createSession($pdo, 'test', 'pass2');
+
+        $this->assertEquals($session['user_id'], $session2['user_id']);
+
+    }
+}
